0a21d7d6b43160e40e5d167ad0bbdc77e4909697
willowcheng <e.wingus@Gmail.com>
2015-09-27 00:20:41 -0400

    Processing JSON data

--- stage2/video4/js/widget.js
+++ stage2/video4/js/widget.js
@@ -1,8 +1,19 @@
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
-    if(xhr.readyState === 4){
+    if (xhr.readyState === 4) {
         var employees = JSON.parse(xhr.responseText);
-        console.log(employees);
+        var statusHTML = '<ul class="bulleted">';
+        for (var i = 0; i < employees.length; i += 1) {
+            if (employees[i].inoffice === true) {
+                statusHTML += '<li class="in">';
+            } else {
+                statusHTML += '<li class="out">';
+            }
+            statusHTML += employees[i].name;
+            statusHTML += '</li>';
+        }
+        statusHTML += '</ul>';
+        document.getElementById('employeeList').innerHTML = statusHTML;
     }
 };
 xhr.open('GET', 'data/employees.json');
