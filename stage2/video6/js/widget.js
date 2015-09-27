21a83ed7c47a0eec0395b2618be90a3dc72343de
willowcheng <e.wingus@Gmail.com>
2015-09-27 00:43:02 -0400

    Add meeting room AJAX request

--- stage2/video6/js/widget.js
+++ stage2/video6/js/widget.js
@@ -1,20 +1,41 @@
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
-  if(xhr.readyState === 4 && xhr.status === 200) {
-    var employees = JSON.parse(xhr.responseText);
-    var statusHTML = '<ul class="bulleted">';
-    for (var i=0; i<employees.length; i += 1) {
-      if (employees[i].inoffice === true) {
-        statusHTML += '<li class="in">';
-      } else {
-        statusHTML += '<li class="out">';
-      }
-      statusHTML += employees[i].name;
-      statusHTML += '</li>';
+    if (xhr.readyState === 4) {
+        var employees = JSON.parse(xhr.responseText);
+        var statusHTML = '<ul class="bulleted">';
+        for (var i = 0; i < employees.length; i += 1) {
+            if (employees[i].inoffice === true) {
+                statusHTML += '<li class="in">';
+            } else {
+                statusHTML += '<li class="out">';
+            }
+            statusHTML += employees[i].name;
+            statusHTML += '</li>';
+        }
+        statusHTML += '</ul>';
+        document.getElementById('employeeList').innerHTML = statusHTML;
     }
-    statusHTML += '</ul>';
-    document.getElementById('employeeList').innerHTML = statusHTML;
-  }
 };
-xhr.open('GET', '../data/employees.json');
+xhr.open('GET', 'data/employees.json');
 xhr.send();
+
+var xhr2 = new XMLHttpRequest();
+xhr2.onreadystatechange = function () {
+    if (xhr2.readyState === 4) {
+        var rooms = JSON.parse(xhr2.responseText);
+        var statusHTML = '<ul class="rooms">';
+        for (var i = 0; i < rooms.length; i += 1) {
+            if(rooms[i].available === true) {
+                statusHTML += '<li class="empty">';
+            } else {
+                statusHTML += '<li class="full">';
+            }
+            statusHTML += rooms[i].room;
+            statusHTML += '</li>';
+        }
+        statusHTML += '</ul>';
+        document.getElementById('roomList').innerHTML = statusHTML;
+    }
+};
+xhr2.open('GET', 'data/rooms.json');
+xhr2.send();
