0a05944908e7ff9d3d6281baba4ecb8bbe1812b2
willowcheng <e.wingus@Gmail.com>
2015-10-16 16:43:01 -0400

    Display photos

--- stage4/video4/js/flickr.js
+++ stage4/video4/js/flickr.js
@@ -9,9 +9,18 @@ $(document).ready(function () {
             tags: animal,
             format: "json"
         };
+
         function displayPhotos(data) {
-            
+            var photoHTML = '<ul>';
+            $.each(data.items, function (i, photo) {
+                photoHTML += '<li class="grid-25 tablet-grid-25">';
+                photoHTML += '<a href="' + photo.link + '" class="image">';
+                photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+            });
+            photoHTML += '</ul>';
+            $("#photos").html(photoHTML);
         }
+
         $.getJSON(flickrAPI, flickrOptions, displayPhotos);
     });
 }); // end ready
