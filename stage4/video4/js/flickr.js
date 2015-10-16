c8c3b87b8951721cfac995d9df380fe8423bb93f
willowcheng <e.wingus@Gmail.com>
2015-10-16 16:14:48 -0400

    Make the AJAX request

--- stage4/video4/js/flickr.js
+++ stage4/video4/js/flickr.js
@@ -2,5 +2,16 @@ $(document).ready(function () {
     $('button').click(function () {
         $("button").removeClass("selected");
         $(this).addClass("selected");
+
+        var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+        var animal = $(this).text();
+        var flickrOptions = {
+            tags: animal,
+            format: "json"
+        };
+        function displayPhotos(data) {
+            
+        }
+        $.getJSON(flickrAPI, flickrOptions, displayPhotos);
     });
 }); // end ready
