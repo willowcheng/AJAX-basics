f1c0a5899566de68865f42c23e5ff0b3893e83f5
willowcheng <e.wingus@Gmail.com>
2015-10-16 16:55:27 -0400

    Use AJAX for forum challenge

--- stage4/video7/js/app.js
+++ stage4/video7/js/app.js
@@ -1,31 +1,36 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
- $('button').click(function () {
-    // highlight the button
-    // not AJAX, just cool looking
-    $("button").removeClass("selected");
-    $(this).addClass("selected");
+    $('form').submit(function (event) {
+        event.preventDefault();
+        var $searchField = $('#search');
+        var $submitButton = $('#submit');
 
-    // the AJAX part
-    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-    var animal = $(this).text();
-    var flickrOptions = {
-      tags: animal,
-      format: "json"
-    };
-    function displayPhotos(data) {
-      var photoHTML = '<ul>';
-      $.each(data.items,function(i,photo) {
-        photoHTML += '<li class="grid-25 tablet-grid-50">';
-        photoHTML += '<a href="' + photo.link + '" class="image">';
-        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
-      }); // end each
-      photoHTML += '</ul>';
-      $('#photos').html(photoHTML);
-    }
-    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
+        $searchField.prop("disabled", true);
+        $submitButton.attr("disabled", true).val("Searching....");
+        // the AJAX part
+        var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+        var animal = $searchField.val();
+        var flickrOptions = {
+            tags: animal,
+            format: "json"
+        };
 
-  }); // end click
+        function displayPhotos(data) {
+            var photoHTML = '<ul>';
+            $.each(data.items, function (i, photo) {
+                photoHTML += '<li class="grid-25 tablet-grid-50">';
+                photoHTML += '<a href="' + photo.link + '" class="image">';
+                photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+            }); // end each
+            photoHTML += '</ul>';
+            $('#photos').html(photoHTML);
+            $searchField.prop("disabled", false);
+            $submitButton.attr("disabled", false).val("Search");
+        }
+
+        $.getJSON(flickerAPI, flickrOptions, displayPhotos);
+
+    }); // end click
 
 }); // end ready
